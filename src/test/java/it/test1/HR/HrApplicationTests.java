33b779899ab8db71d60893893abd07b678fad721
Ivan Sbaiz <sbaiz1984@gmail.com>
2022-08-25 19:54:03 +0200

    first own project, first commit

---
+++ src/test/java/it/test1/HR/HrApplicationTests.java
@@ -0,0 +1,13 @@
+package it.test1.HR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
